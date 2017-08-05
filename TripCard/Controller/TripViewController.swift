c0913345d169a0ca0cc4f85f47b7b7de534723e5
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-08-05 14:46:22 +0300

    Modified Model for Parse DataBase.

--- TripCard/Controller/TripViewController.swift
+++ TripCard/Controller/TripViewController.swift
@@ -13,15 +13,7 @@ class TripViewController: UIViewController, UICollectionViewDelegate, UICollecti
     @IBOutlet var backgroundImageView: UIImageView!
     @IBOutlet var collectionView: UICollectionView!
     
-    private var trips = [Trip(tripId: "Paris001", city: "Paris", country: "France", featuredImage: UIImage(named: "paris"), price: 2000, totalDays: 5, isLiked: false),
-                         Trip(tripId: "Rome001", city: "Rome", country: "Italy", featuredImage: UIImage(named: "rome"), price: 800, totalDays: 3, isLiked: false),
-                         Trip(tripId: "Istanbul001", city: "Istanbul", country: "Turkey", featuredImage: UIImage(named: "istanbul"), price: 2200, totalDays: 10, isLiked: false),
-                         Trip(tripId: "London001", city: "London", country: "United Kingdom", featuredImage: UIImage(named: "london"), price: 3000, totalDays: 4, isLiked: false),
-                         Trip(tripId: "Sydney001", city: "Sydney", country: "Australia", featuredImage: UIImage(named: "sydney"), price: 2500, totalDays: 8, isLiked: false),
-                         Trip(tripId: "Santorini001", city: "Santorini", country: "Greece", featuredImage: UIImage(named: "santorini"), price: 1800, totalDays: 7, isLiked: false),
-                         Trip(tripId: "NewYork001", city: "New York", country: "United States", featuredImage: UIImage(named: "newyork"), price: 900, totalDays: 3, isLiked: false),
-                         Trip(tripId: "Kyoto001", city: "Kyoto", country: "Japan", featuredImage: UIImage(named: "kyoto"), price: 1000, totalDays: 5, isLiked: false)
-    ]
+    private var trips = [Trip]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- TripCard/Model/Trip.swift
+++ TripCard/Model/Trip.swift
@@ -7,17 +7,18 @@
 //
 
 import UIKit
+import Parse
 
 struct Trip {
     var tripId = ""
     var city = ""
     var country = ""
-    var featuredImage: UIImage?
+    var featuredImage: PFFile?
     var price: Int = 0
     var totalDays: Int = 0
     var isLiked = false
     
-    init(tripId: String, city: String, country: String, featuredImage: UIImage!, price: Int, totalDays: Int, isLiked: Bool) {
+    init(tripId: String, city: String, country: String, featuredImage: PFFile!, price: Int, totalDays: Int, isLiked: Bool) {
         self.tripId = tripId
         self.city = city
         self.country = country
@@ -26,4 +27,39 @@ struct Trip {
         self.totalDays = totalDays
         self.isLiked = isLiked
     }
+    
+    init(pfObject: PFObject) {
+        self.tripId = pfObject.objectId!
+        self.city = pfObject["city"] as! String
+        self.country = pfObject["country"] as! String
+        self.price = pfObject["price"] as! Int
+        self.totalDays = pfObject["totalDays"] as! Int
+        self.featuredImage = pfObject["featuredImage"] as? PFFile
+        self.isLiked = pfObject["isLiked"] as! Bool
+    }
+    
+    func toPFObject() -> PFObject {
+        let tripObject = PFObject(className: "Trip")
+        tripObject.objectId = tripId
+        tripObject["city"] = city
+        tripObject["country"] = country
+        tripObject["featuredImage"] = featuredImage
+        tripObject["price"] = price
+        tripObject["totalDays"] = totalDays
+        tripObject["isLiked"] = isLiked
+        return tripObject
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
