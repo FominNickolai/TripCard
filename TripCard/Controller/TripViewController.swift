ad0c7cd74b1993ca57d9a67abff35bc577c95e75
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-08-05 12:28:21 +0300

    Add Parse pod. Add frameworks to work with Parse.

--- TripCard/Controller/TripViewController.swift
+++ TripCard/Controller/TripViewController.swift
@@ -2,44 +2,30 @@
 //  TripViewController.swift
 //  TripCard
 //
-//  Created by Simon Ng on 8/11/2016.
-//  Copyright © 2016 AppCoda. All rights reserved.
+//  Created by Fomin Mykola on 8/5/17.
+//  Copyright © 2017 AppCoda. All rights reserved.
 //
 
 import UIKit
 
-class TripViewController: UIViewController {
-
+class TripViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, TripCollectionCellDelegate {
+    
     @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var collectionView: UICollectionView!
     
-    @IBOutlet weak var collectionView: UICollectionView!
-    
-    fileprivate var trips = [Trip(tripId: "Paris001", city:
-    "Paris", country: "France", featuredImage: UIImage(named:
-    "paris"), price: 2000, totalDays: 5, isLiked: false),
-    
-    Trip(tripId: "Rome001", city: "Rome", country: "Italy",
-    featuredImage: UIImage(named: "rome"), price: 800,
-    totalDays: 3, isLiked: false),
-    
-    Trip(tripId: "Istanbul001", city: "Istanbul", country:
-    "Turkey", featuredImage: UIImage(named: "istanbul"), price:
-    2200, totalDays: 10, isLiked: false),
-    
-    Trip(tripId: "London001", city: "London", country: "United Kingdom", featuredImage: UIImage(named: "london"),
-    price: 3000, totalDays: 4, isLiked: false),
-    Trip(tripId: "Sydney001", city: "Sydney", country: "Australia", featuredImage: UIImage(named: "sydney"), price: 2500, totalDays: 8, isLiked: false),
-    
-    Trip(tripId: "Santorini001", city: "Santorini", country: "Greece", featuredImage: UIImage(named: "santorini"), price: 1800, totalDays: 7, isLiked: false),
-    
-    Trip(tripId: "NewYork001", city: "New York", country: "United States", featuredImage: UIImage(named: "newyork"), price: 900, totalDays: 3, isLiked: false),
-    
-    Trip(tripId: "Kyoto001", city: "Kyoto", country: "Japan", featuredImage: UIImage(named: "kyoto"), price: 1000, totalDays: 5, isLiked: false)
+    private var trips = [Trip(tripId: "Paris001", city: "Paris", country: "France", featuredImage: UIImage(named: "paris"), price: 2000, totalDays: 5, isLiked: false),
+                         Trip(tripId: "Rome001", city: "Rome", country: "Italy", featuredImage: UIImage(named: "rome"), price: 800, totalDays: 3, isLiked: false),
+                         Trip(tripId: "Istanbul001", city: "Istanbul", country: "Turkey", featuredImage: UIImage(named: "istanbul"), price: 2200, totalDays: 10, isLiked: false),
+                         Trip(tripId: "London001", city: "London", country: "United Kingdom", featuredImage: UIImage(named: "london"), price: 3000, totalDays: 4, isLiked: false),
+                         Trip(tripId: "Sydney001", city: "Sydney", country: "Australia", featuredImage: UIImage(named: "sydney"), price: 2500, totalDays: 8, isLiked: false),
+                         Trip(tripId: "Santorini001", city: "Santorini", country: "Greece", featuredImage: UIImage(named: "santorini"), price: 1800, totalDays: 7, isLiked: false),
+                         Trip(tripId: "NewYork001", city: "New York", country: "United States", featuredImage: UIImage(named: "newyork"), price: 900, totalDays: 3, isLiked: false),
+                         Trip(tripId: "Kyoto001", city: "Kyoto", country: "Japan", featuredImage: UIImage(named: "kyoto"), price: 1000, totalDays: 5, isLiked: false)
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Apply blurring effect
         backgroundImageView.image = UIImage(named: "cloud")
         let blurEffect = UIBlurEffect(style: .dark)
@@ -47,15 +33,15 @@ class TripViewController: UIViewController {
         blurEffectView.frame = view.bounds
         backgroundImageView.addSubview(blurEffectView)
         
-        collectionView.backgroundColor = .clear
+        // Make the colleciton view transparent
+        collectionView.backgroundColor = UIColor.clear
         
         if UIScreen.main.bounds.size.height == 568.0 {
             let flowLayout = self.collectionView.collectionViewLayout as! UICollectionViewFlowLayout
             flowLayout.itemSize = CGSize(width: 250.0, height: 330.0)
         }
-        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -65,9 +51,8 @@ class TripViewController: UIViewController {
         return .lightContent
     }
     
-}
-
-extension TripViewController: UICollectionViewDataSource {
+    // MARK: - Collection view delegate
+    
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
@@ -79,7 +64,7 @@ extension TripViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! TripCollectionViewCell
         
-        //Configure Cell
+        // Configure the cell
         cell.cityLabel.text = trips[indexPath.row].city
         cell.countryLabel.text = trips[indexPath.row].country
         cell.imageView.image = trips[indexPath.row].featuredImage
@@ -88,18 +73,14 @@ extension TripViewController: UICollectionViewDataSource {
         cell.isLiked = trips[indexPath.row].isLiked
         cell.delegate = self
         
-        //Apply round corner
+        // Apply round corner
         cell.layer.cornerRadius = 4.0
         
         return cell
     }
-}
-
-extension TripViewController: UICollectionViewDelegate {
     
-}
-
-extension TripViewController: TripCollectionCellDelegate {
+    // MARK: - TripCollectionCellDelegate methods
+    
     func didLikeButtonPressed(cell: TripCollectionViewCell) {
         if let indexPath = collectionView.indexPath(for: cell) {
             trips[indexPath.row].isLiked = trips[indexPath.row].isLiked ? false : true
@@ -107,36 +88,5 @@ extension TripViewController: TripCollectionCellDelegate {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
